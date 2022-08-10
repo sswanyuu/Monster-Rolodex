7236f97046cddfb1d3207f1dbb066aeed162d390
WanYu <yosoywanyushih@gmail.com>
2022-08-10 12:20:48 -0400

    Transfer class components into functional components

--- src/App.js
+++ src/App.js
@@ -1,61 +1,68 @@
 import "./App.css";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box";
-class App extends Component {
-  constructor() {
-    super();
-    //this state object should be always a json object
-    //set the initial value
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-    console.log("constructor");
-  }
-  componentDidMount() {
-    //a promise is asynchonous in javaScript
-    //get the data from this url
-    console.log("componentdidmount!");
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filterMonsters, setFilterMonsters] = useState(monsters);
+  //a promise is asynchonous in javaScript
+  //get the data from this url
+  //fetch from foreign API
+  useEffect(() => {
+    console.log("Effect is triggered");
     fetch("https://jsonplaceholder.typicode.com/users")
       //tranlate the data into json type
       .then((response) => response.json())
-      .then((users) => {
-        this.setState(() => {
-          return { monsters: users };
-        });
-      });
-  }
-  //just initialized once (optimized method)
-  handleSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-  //what to show
-  render() {
-    //using destructuring
-    const { monsters, searchField } = this.state;
-    const { handleSearchChange } = this;
-    console.log("render");
-    //generate new array for the filter
-    //inmutabability
-    const filterMonsters = monsters.filter((monster) => {
+      .then((users) => setMonsters(users));
+  }, []);
+  useEffect(() => {
+    const newFilterMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchField);
     });
-    return (
-      <div className="App">
-        <h1 className="title">Get A Partner of Your Life</h1>
-        <SearchBox
-          className="search-box"
-          onChangeHandler={handleSearchChange}
-          placeHolder="Search monsters..."
-        />
-        <CardList monsters={filterMonsters} />
-      </div>
-    );
-  }
-}
+    setFilterMonsters(newFilterMonsters);
+  }, [monsters, searchField]);
+  //     //generate new array for the filter
+  //     //inmutabability
 
+  const handleSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    //when it detect that the stated value change, render.
+    setSearchField(searchFieldString);
+  };
+  return (
+    <div className="App">
+      <h1 className="title">Get A Partner of Your Life</h1>
+      <SearchBox
+        className="search-box"
+        onChangeHandler={handleSearchChange}
+        placeHolder="Search monsters..."
+      />
+      <CardList monsters={filterMonsters} />
+    </div>
+  );
+  //class App extends Component {
+  //   constructor() {
+  //     super();
+  //     //this state object should be always a json object
+  //     //set the initial value
+  //     this.state = {
+  //       monsters: [],
+  //       searchField: "",
+  //     };
+  //     console.log("constructor");
+  //   }
+
+  //   }
+  //   //just initialized once (optimized method)
+
+  //   //what to show
+  //   render() {
+  //     //using destructuring
+  //
+  //
+  //   }
+  // }
+};
 export default App;
--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Component } from "react";
 //the css will be present on the entire app
 import "./search-box.styles.css";
-class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        //the input has two className
-        className={`search-box ${this.props.className}`}
-        type="search"
-        placeholder={this.props.placeHolder}
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const SearchBox = ({ className, placeHolder, onChangeHandler }) => {
+  return (
+    <input
+      //the input has two className
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeHolder}
+      onChange={onChangeHandler}
+    />
+  );
+};
+
 export default SearchBox;
